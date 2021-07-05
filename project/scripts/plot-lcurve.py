7efcfbe63f1ed9b04ec9a9569132b6f786df389e
Chenxing Luo <chenxing.luo@gmail.com>
2021-07-04 23:40:28 -0500

    Change plot-lcurve to support custom file name and on/off switch for trn/tst

--- project/scripts/plot-lcurve.py
+++ project/scripts/plot-lcurve.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import pandas
 from matplotlib import colors, pyplot as plt
 import click
@@ -10,6 +11,10 @@ KEYS = ["l2_tst", "l2_trn", "l2_e_tst", "l2_e_trn", "l2_f_tst", "l2_f_trn", "lr"
 
 @click.command()
 @click.argument("lcurves", nargs=-1)
+@click.option("--train/--no-train", default=True)
+@click.option("--test/--no-test", default=True)
+@click.option("-n", "--num-steps", default=None, type=click.INT)
+@click.option("-o", "--output", type=click.Path(), help="Output file name.", default="lcurve.png")
 def main(**kwargs):
 
     fig, axes = plt.subplots(2, 2, sharex=True)
@@ -30,14 +35,23 @@ def main(**kwargs):
                 KEYS[4]: axes[1,0], KEYS[5]: axes[1,0],
                 KEYS[6]: axes[1,1],
             }[colname]
+
+            if kwargs.get("train") == False and colname.endswith("trn"): continue
+            if kwargs.get("test") == False and colname.endswith("tst"): continue
+
+            num_steps = kwargs.get("num_steps")
+            if num_steps:
+                df = df.loc[:num_steps]
+
+
             df.reset_index().plot.scatter(
-                x="# batch", y=colname, ax=ax, label=label, color=color, marker=("x" if colname.endswith("tst") else "o"))
+                x="# batch", y=colname, ax=ax, label=label, color=color, marker=("x" if colname.endswith("tst") else "o"), alpha=.75)
             ax.semilogy()
 
             if colname != "lr":
                 ax.get_legend().remove()
     
-    plt.savefig("lcurve.png", dpi=144)
+    plt.savefig(kwargs.get("output"), dpi=144)
 
 if __name__ == "__main__":
     main()
