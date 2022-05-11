3e5fc11407e87882a098696a45b15114566538a6
Chenxing Luo <chenxing.luo@gmail.com>
2022-05-11 15:33:31 -0400

    Update plot-lcurve to support dpmd v2 output

--- project/scripts/plot-lcurve.py
+++ project/scripts/plot-lcurve.py
@@ -8,7 +8,14 @@ from io import StringIO
 
 
 COLORS = plt.rcParams["axes.prop_cycle"].by_key()["color"]
-KEYS = ["l2_tst", "l2_trn", "l2_e_tst", "l2_e_trn", "l2_f_tst", "l2_f_trn", "lr"]
+
+KEYS = [
+    "rmse_val", "rmse_trn",
+    "rmse_e_val", "rmse_e_trn",
+    "rmse_f_val", "rmse_f_trn",
+    "lr",
+]
+
 
 @click.command()
 @click.argument("lcurves", nargs=-1)
@@ -18,7 +25,7 @@ KEYS = ["l2_tst", "l2_trn", "l2_e_tst", "l2_e_trn", "l2_f_tst", "l2_f_trn", "lr"
 @click.option("-o", "--output", type=click.Path(), help="Output file name.", default="lcurve.png")
 def main(**kwargs):
 
-    fig, axes = plt.subplots(2, 2, sharex=True)
+    fig, axes = plt.subplots(2, 2, sharex=True, figsize=(8,6))
 
     # colname = kwargs.pop("colname")
     lcurves = kwargs.pop("lcurves", [])
@@ -30,10 +37,13 @@ def main(**kwargs):
 
         with open(fname) as fp:
             lines = fp.readlines()
-            lines = [line for idx, line in enumerate(lines) if idx == 0 or "batch" not in line]
+        lines = [line.strip() for idx, line in enumerate(lines) if (idx != 0) and ("#" not in line)]
         fp = StringIO("\n".join(lines))
+        df = pandas.read_table(fp, sep=r"\s+", index_col=0, header=None)
+        df.columns = KEYS
+        df.index.name = "#"
 
-        df = pandas.read_table(fp, sep=r"\s{2,}", header=0, index_col=0)
+
         for colname in KEYS:
             ax = {
                 KEYS[0]: axes[0,0], KEYS[1]: axes[0,0],
@@ -49,14 +59,14 @@ def main(**kwargs):
             if num_steps:
                 df = df.loc[:num_steps]
 
-
-            df.reset_index().plot.scatter(
-                x="# batch", y=colname, ax=ax, label=label, color=color, marker=("x" if colname.endswith("tst") else "o"), alpha=.75)
+            _ = df.reset_index()
+            _.plot.scatter(
+                x=_.columns[0], y=colname, ax=ax, label=label, color=color, marker=("x" if colname.endswith("tst") else "o"), alpha=.75, s=4)
             ax.semilogy()
 
             if colname != "lr":
                 ax.get_legend().remove()
-    
+
     plt.savefig(kwargs.get("output"), dpi=144)
 
 if __name__ == "__main__":
